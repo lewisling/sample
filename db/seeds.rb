060d8fcc418728d33fc073b79148e4fc717880c1
Li SUN <SunLi@MrSunLi.com>
2012-10-17 15:41:05 +0800

    db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+
+Category.destroy_all
+["C35 PAV", "C52 In-Car", "C36 Docking", "C40 Clock", "E56 DV", "C77 HMP",
+  "D75 Tablet", "C54 Micro"].each do |name|
+  Category.create(:name => name)
+end
+
+SwClass.destroy_all
+["A", "B", "C"].each do |name|
+  SwClass.create(:level => name)
+end
+
+Swa.destroy_all
+["Terence", "SUN Li", "Bret", "Norman", "Lewis", "Ricky", "Simon",
+  "Sanjay"].each do |name|
+  Swa.create(:name => name)
+end
+
+Spl.destroy_all
+["Ronald", "Jacky", "Elong", "John", "Johnson", "Simon", "Ricky",
+  "YuDong", "Tony"].each do |name|
+  Spl.create(:name => name)
+end
+
+Supplier.destroy_all
+["KH", "TeamForce", "ForYou", "TCL", "Neon", "Yusan", "Arts", "ASD", "Chicony",
+  "Computime", "Vtrek"].each do |name|
+  Supplier.create(:name => name)
+end
+
+ReviewPurpose.destroy_all
+["Pre-Milstone", "Post-Milestone"].each do |purpose|
+  ReviewPurpose.create(:purpose => purpose)
+end
+
+ReviewResult.destroy_all
+["Pass", "Not Pass"].each do |result|
+  ReviewResult.create(:conclusion => result)
+end
+
+Milestone.destroy_all
+["PD", "VPD", "AA", "PPC", "PV", "CR"].each do |name|
+  Milestone.create(:name => name)
+end
