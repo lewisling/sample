02acb7e424bef3ee4a945169f6d7e42a684c05e9
Li SUN <SunLi@MrSunLi.com>
2012-11-01 10:52:40 +0800

    presence

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ActiveRecord::Base
   attr_accessible :aa_actual, :aa_plan, :active, :category_id, :comment, :cr_actual, :cr_plan, :description, :folder_ready, :handover, :model, :pd_actual, :pd_plan, :platform_id, :ppc_actual, :ppc_plan, :pv_actual, :pv_plan, :spl_id, :supplier_id, :swa_id, :swclass_id, :volume_forecast, :vpd_actual, :vpd_plan
 
+  validates :category_id, :model, :spl_id, :swa_id, :swclass_id, :platform_id, :supplier_id, :presence => true
+
   belongs_to :category
   belongs_to :swclass
   belongs_to :platform
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,8 @@
 class Review < ActiveRecord::Base
   attr_accessible :actual_date, :comment, :milestone_id, :planned_date, :product_id, :reviewresult_id, :reviewpurpose_id
 
+  validates :product_id, :milestone_id, :reviewpurpose_id, :reviewresult_id, :presence => true
+
   belongs_to :product
   belongs_to :milestone
   belongs_to :reviewpurpose
